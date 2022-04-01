bdbcdf80e88ea0f55d8a0e0f93ae9ef75b1620fb
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-04-01 17:33:50 +0200

    Concert

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -8,16 +8,6 @@ class GildedRose(var items: Array<Item>) {
         }
     }
 
-    private fun updateAgedBrie(item: Item, currentSellIn: Int) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1
-        }
-        if (currentSellIn <= 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-            }
-        }
-    }
 
     private fun updateQualityOfItem(item: Item) {
         if (item.name == "Sulfuras, Hand of Ragnaros") {
@@ -25,44 +15,53 @@ class GildedRose(var items: Array<Item>) {
         }
         val currentSellIn = item.sellIn
         updateSellIn(item)
-
         if (item.name == "Aged Brie") {
             updateAgedBrie(item, currentSellIn)
             return
         }
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            updateConcert(item, currentSellIn)
+            return
+        }
 
-        if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1
+        }
+        if (currentSellIn <= 0) {
             if (item.quality > 0) {
                 item.quality = item.quality - 1
             }
-        } else {
+        }
+    }
+
+    private fun updateConcert(item: Item, currentSellIn: Int) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+        if (currentSellIn < 11) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1
-
-                if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                    if (currentSellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-
-                    if (currentSellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                }
             }
         }
 
+        if (currentSellIn < 6) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
 
         if (currentSellIn <= 0) {
-            if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1
-                }
-            } else {
-                item.quality = item.quality - item.quality
+            item.quality = item.quality - item.quality
+        }
+    }
+
+    private fun updateAgedBrie(item: Item, currentSellIn: Int) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+        if (currentSellIn <= 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
             }
         }
     }
